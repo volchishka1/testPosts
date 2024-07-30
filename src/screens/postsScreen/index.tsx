55875f4e82aa62284bcbde7e94ab4292ac16e81f
macwolf <romazverb18@gmsil.com>
2024-07-31 00:11:11 +0300

    added some functions to addPost

--- src/screens/postsScreen/index.tsx
+++ src/screens/postsScreen/index.tsx
@@ -1,9 +1,13 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { PostsScreenView } from '@screens/postsScreen/postsScreenView.tsx';
 import { ROUTES } from '@constants/routes.ts';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { CompositeScreenProps } from '@react-navigation/native';
 import { NativeStackNavigatorParamList } from '@navigation/types.ts';
+import { useDispatch } from 'react-redux';
+import { setBody, setPostData, setTitle } from '@root/store/actions.ts';
+import { useAppSelector } from '@root/hooks/hooks.ts';
+import { getBody, getPostsData, getTitle } from '@root/store/homeScreen/selectors.ts';
 
 export type PostsScreenProps = CompositeScreenProps<
   NativeStackScreenProps<NativeStackNavigatorParamList, ROUTES.POSTS_SCREEN>,
@@ -12,9 +16,61 @@ export type PostsScreenProps = CompositeScreenProps<
 
 export const PostsScreen: FC<PostsScreenProps> = (props) => {
   const { navigation } = props;
+  const dispatch = useDispatch();
+  const getTitleData = useAppSelector(getTitle);
+  const getBodyData = useAppSelector(getBody);
+  const getPosts = useAppSelector(getPostsData);
+  const [post, setPost] = useState<
+    {
+      id: number;
+      title: string;
+      body: string;
+    }[]
+  >([]);
 
   const navigationToPostScreen = () => {
     navigation.navigate(ROUTES.POST_SCREEN);
   };
-  return <PostsScreenView navigationToPostScreen={navigationToPostScreen} />;
+
+  useEffect(() => {
+    const posts = getPosts.map((item) => {
+      const title = item.title;
+      const body = item.body;
+      const id = item.id;
+      return {
+        title,
+        body,
+        id,
+      };
+    });
+    setPost(posts);
+    console.log(posts);
+  }, [getPosts]);
+
+  const setTitleData = (title: string) => {
+    dispatch(setTitle(title));
+  };
+  const setBodyData = (body: string) => {
+    dispatch(setBody(body));
+  };
+
+  const addPost = () => {
+    dispatch(
+      setPostData({
+        id: 1 + Math.max(0, ...post.map((n) => n.id)),
+        title: getTitleData,
+        body: getBodyData,
+      }),
+    );
+  };
+
+  return (
+    <PostsScreenView
+      navigationToPostScreen={navigationToPostScreen}
+      setTitle={setTitleData}
+      setBody={setBodyData}
+      addPost={addPost}
+      post={post}
+    />
+  );
 };
