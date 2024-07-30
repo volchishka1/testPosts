ca0a9cd6f42ee67f0ee6e3226a6a1f01ce20682e
macwolf <romazverb18@gmsil.com>
2024-07-31 00:11:23 +0300

    show addPost

--- src/screens/postsScreen/postsScreenView.tsx
+++ src/screens/postsScreen/postsScreenView.tsx
@@ -1,13 +1,30 @@
 import React, { FC } from 'react';
-import { View } from 'react-native';
+import { FlatList, Text, TextInput, View } from 'react-native';
 import { PostsScreenViewProps } from '@screens/postsScreen/types.ts';
 import { CustomButton } from '@root/comoinents/buttonComponent/customButtonComponent.tsx';
+import { postsScreenViewStyles } from '@screens/postsScreen/styles.ts';
 
 export const PostsScreenView: FC<PostsScreenViewProps> = (props) => {
-  const { navigationToPostScreen } = props;
+  const { navigationToPostScreen, setTitle, setBody, addPost, post } = props;
   return (
     <View>
-      <CustomButton onPress={navigationToPostScreen} textButton={'Перейти'} />
+      <View style={postsScreenViewStyles.createPostContainer}>
+        <View style={postsScreenViewStyles.textInputContainer}>
+          <TextInput style={postsScreenViewStyles.textInput} onChangeText={setTitle} />
+          <TextInput style={postsScreenViewStyles.textInput} onChangeText={setBody} />
+        </View>
+        <CustomButton onPress={addPost} textButton={'Добавить'} />
+      </View>
+      <FlatList
+        style={postsScreenViewStyles.postsContainer}
+        data={post}
+        renderItem={({ item }) => (
+          <View style={postsScreenViewStyles.postContainer}>
+            <Text>{item.title}</Text>
+            <Text>{item.body}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
