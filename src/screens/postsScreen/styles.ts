28455546183d55bf3c0835acd1f54bf0039c0f97
macwolf <romazverb18@gmsil.com>
2024-07-31 00:11:41 +0300

    added types and styles to postScreen

--- src/screens/postsScreen/styles.ts
+++ src/screens/postsScreen/styles.ts
@@ -1,5 +1,39 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ViewStyle } from 'react-native';
 
-export type PostsScreenStyles = {};
+export type PostsScreenStyles = {
+  createPostContainer: ViewStyle;
+  textInputContainer: ViewStyle;
+  textInput: ViewStyle;
+  postsContainer: ViewStyle;
+  postContainer: ViewStyle;
+};
 
-export const postsScreenViewStyles = StyleSheet.create<PostsScreenStyles>({});
+export const postsScreenViewStyles = StyleSheet.create<PostsScreenStyles>({
+  createPostContainer: {
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 120,
+    marginVertical: 8,
+    flexDirection: 'row',
+  },
+  textInputContainer: {
+    width: 180,
+  },
+  textInput: {
+    width: '100%',
+    height: 25,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 2,
+  },
+  postsContainer: {
+    borderRadius: 10,
+    backgroundColor: '#e5c5f8',
+  },
+  postContainer: {
+    alignItems: 'flex-start',
+    height: 40,
+  },
+});
--- src/screens/postsScreen/types.ts
+++ src/screens/postsScreen/types.ts
@@ -1,5 +1,9 @@
-import { TouchableOpacityProps } from 'react-native';
+import { TextInputProps, TouchableOpacityProps } from 'react-native';
 
 export type PostsScreenViewProps = {
   navigationToPostScreen: TouchableOpacityProps['onPress'];
+  addPost: TouchableOpacityProps['onPress'];
+  setTitle: TextInputProps['onChangeText'];
+  setBody: TextInputProps['onChangeText'];
+  post: TextInputProps['children'];
 };
