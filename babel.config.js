1f11159a8ede39cc84559053d00826596e909ea7
macwolf <romazverb18@gmsil.com>
2024-07-30 11:39:19 +0300

    added babel settings

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,28 @@
+const path = require('path');
+
+const root = path.resolve(__dirname);
+
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@assets': `${root}/assets`,
+          '@components': `${root}/src/components`,
+          '@config': `${root}/src/config`,
+          '@constants': `${root}/src/constants`,
+          '@navigation': `${root}/src/navigation`,
+          '@root': `${root}/src`,
+          '@screens': `${root}/src/screens`,
+          '@state': `${root}/src/state`,
+        },
+        extensions: ['.android.js', '.ios.js', '.js', '.json', '.ts', '.tsx'],
+        root: [root],
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
