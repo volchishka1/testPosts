c706ebe0fc8c7a7713fe4300be85bf464771c179
macwolf <romazverb18@gmsil.com>
2024-07-31 00:10:12 +0300

    added provider to app

--- App.tsx
+++ App.tsx
@@ -10,12 +10,16 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { Navigator } from './src/navigation/navigator';
+import { Provider } from 'react-redux';
+import { store } from './src/store/store';
 
 function App() {
   return (
-    <SafeAreaProvider>
-      <Navigator />
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <Navigator />
+      </SafeAreaProvider>
+    </Provider>
   );
 }
 
