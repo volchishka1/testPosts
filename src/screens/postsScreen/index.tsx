cd581ceb7dfc8472e0612153ce2d1da4eea1974a
macwolf <romazverb18@gmsil.com>
2024-08-08 01:22:37 +0300

    added ItemDataType, loadData selector, setPosts function

--- src/screens/postsScreen/index.tsx
+++ src/screens/postsScreen/index.tsx
@@ -5,21 +5,28 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { CompositeScreenProps } from '@react-navigation/native';
 import { NativeStackNavigatorParamList } from '@navigation/types.ts';
 import { useDispatch } from 'react-redux';
-import { setBody, setPostData, setTitle } from '@root/store/actions.ts';
+import { saveData, setBody, setPostBody, setPostTitle, setTitle } from '@root/store/actions.ts';
 import { useAppSelector } from '@root/hooks/hooks.ts';
-import { getBody, getPostsData, getTitle } from '@root/store/homeScreen/selectors.ts';
+import { getBody, getLoadData, getTitle } from '@root/store/homeScreen/selectors.ts';
+// import { getData, postData } from '@root/store/api-actions.ts';
 
 export type PostsScreenProps = CompositeScreenProps<
   NativeStackScreenProps<NativeStackNavigatorParamList, ROUTES.POSTS_SCREEN>,
   any
 >;
 
+export type ItemData = {
+  id: number;
+  title: string;
+  body: string;
+};
+
 export const PostsScreen: FC<PostsScreenProps> = (props) => {
   const { navigation } = props;
   const dispatch = useDispatch();
   const getTitleData = useAppSelector(getTitle);
   const getBodyData = useAppSelector(getBody);
-  const getPosts = useAppSelector(getPostsData);
+  const loadData: ItemData[] = useAppSelector(getLoadData);
   const [post, setPost] = useState<
     {
       id: number;
@@ -32,8 +39,15 @@ export const PostsScreen: FC<PostsScreenProps> = (props) => {
     navigation.navigate(ROUTES.POST_SCREEN);
   };
 
+  const setPostData = (title: string, body: string) => {
+    dispatch(setPostTitle(title));
+    dispatch(setPostBody(body));
+    navigationToPostScreen();
+  };
+
   useEffect(() => {
-    const posts = getPosts.map((item) => {
+    // dispatch(getData());
+    const posts = loadData.map((item) => {
       const title = item.title;
       const body = item.body;
       const id = item.id;
@@ -44,8 +58,7 @@ export const PostsScreen: FC<PostsScreenProps> = (props) => {
       };
     });
     setPost(posts);
-    console.log(posts);
-  }, [getPosts]);
+  }, [loadData]);
 
   const setTitleData = (title: string) => {
     dispatch(setTitle(title));
@@ -56,21 +69,22 @@ export const PostsScreen: FC<PostsScreenProps> = (props) => {
 
   const addPost = () => {
     dispatch(
-      setPostData({
+      saveData({
         id: 1 + Math.max(0, ...post.map((n) => n.id)),
         title: getTitleData,
         body: getBodyData,
       }),
+      // postData(1 + Math.max(0, ...post.map((n) => n.id)), getTitleData, getBodyData),
     );
   };
 
   return (
     <PostsScreenView
-      navigationToPostScreen={navigationToPostScreen}
       setTitle={setTitleData}
       setBody={setBodyData}
       addPost={addPost}
       post={post}
+      setPostData={setPostData}
     />
   );
 };
