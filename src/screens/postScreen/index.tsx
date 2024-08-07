0c7da1c064ece8d9c813fc113706c310a327e09f
macwolf <romazverb18@gmsil.com>
2024-08-08 01:21:40 +0300

    added selectors to PostScreen

--- src/screens/postScreen/index.tsx
+++ src/screens/postScreen/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { PostScreenView } from '@screens/postScreen/postScreenView.tsx';
+import { useAppSelector } from '@root/hooks/hooks.ts';
+import { getPostBody, getPostTitle } from '@root/store/homeScreen/selectors.ts';
 
 export const PostScreen = () => {
-  return <PostScreenView />;
+  const getTitleData = useAppSelector(getPostTitle);
+  const getBodyData = useAppSelector(getPostBody);
+
+  return <PostScreenView titleData={getTitleData} bodyData={getBodyData} />;
 };
