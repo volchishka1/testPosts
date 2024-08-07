092a81466698f5130eadb6c1d2ba466c0756ad00
macwolf <romazverb18@gmsil.com>
2024-08-08 01:23:32 +0300

    added show chose post to postScreenView

--- src/screens/postScreen/postScreenView.tsx
+++ src/screens/postScreen/postScreenView.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { PostScreenViewTypes } from '@screens/postScreen/types.ts';
+import { postScreenViewStyles } from '@screens/postScreen/styles.ts';
 import { Text, View } from 'react-native';
 
-export const PostScreenView = () => {
+export const PostScreenView: FC<PostScreenViewTypes> = (props) => {
+  const { titleData, bodyData } = props;
   return (
-    <View>
-      <Text>{'postScreen'}</Text>
+    <View style={postScreenViewStyles.rootContainer}>
+      <View style={postScreenViewStyles.postContainer}>
+        <Text>{titleData}</Text>
+        <Text>{bodyData}</Text>
+      </View>
     </View>
   );
 };
--- src/screens/postsScreen/postsScreenView.tsx
+++ src/screens/postsScreen/postsScreenView.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
-import { FlatList, Text, TextInput, View } from 'react-native';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { PostsScreenViewProps } from '@screens/postsScreen/types.ts';
 import { CustomButton } from '@root/comoinents/buttonComponent/customButtonComponent.tsx';
 import { postsScreenViewStyles } from '@screens/postsScreen/styles.ts';
+import { postScreenViewStyles } from '@screens/postScreen/styles.ts';
 
 export const PostsScreenView: FC<PostsScreenViewProps> = (props) => {
-  const { navigationToPostScreen, setTitle, setBody, addPost, post } = props;
+  const { setTitle, setBody, addPost, post, setPostData } = props;
   return (
     <View style={postsScreenViewStyles.rootContainer}>
       <View style={postsScreenViewStyles.createPostContainer}>
@@ -15,16 +16,21 @@ export const PostsScreenView: FC<PostsScreenViewProps> = (props) => {
         </View>
         <CustomButton onPress={addPost} textButton={'Добавить'} />
       </View>
-      <FlatList
-        style={postsScreenViewStyles.postsContainer}
-        data={post}
-        renderItem={({ item }) => (
-          <View style={postsScreenViewStyles.postContainer}>
-            <Text>{item.title}</Text>
-            <Text>{item.body}</Text>
-          </View>
-        )}
-      />
+      <View style={postsScreenViewStyles.postsContainer}>
+        {post.map((item) => (
+          <TouchableOpacity
+            key={item.id}
+            onPress={() => {
+              setPostData(item.title, item.body);
+            }}
+            style={postScreenViewStyles.rootContainer}>
+            <View style={postScreenViewStyles.postContainer}>
+              <Text>{item.title}</Text>
+              <Text>{item.body}</Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </View>
     </View>
   );
 };
